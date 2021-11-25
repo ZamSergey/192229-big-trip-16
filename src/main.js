31639191362ce876a79bd3bf46706ed68dc0932d
test <zamsergey@gmail.com>
2021-11-25 23:04:57 +0300

    Добавил информацию о поездке. Изменил вывод точек маршрута

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import {createSiteMenuTemplate} from './view/menu.js';
 import {createSiteSortingTemplate} from './view/sorting.js';
 import {editFormTemplate} from './view/edit.js';
 import {addFormTemplate} from './view/add.js';
+import {createTripInfoTemplate} from './view/trip-info.js';
 import {createRoutePoint,createPintsContainer} from './view/route-point.js';
 import {renderTemplate, RenderPosition} from './view/render.js';
 
+const POINT_COUNT = 3;
+
+const tripContainer = document.querySelector('.trip-main');
 const menu = document.querySelector('.trip-controls__navigation');
 const filter = document.querySelector('.trip-controls__filters');
 const sorting = document.querySelector('.trip-events');
 
+renderTemplate(tripContainer,createTripInfoTemplate(),RenderPosition.AFTERBEGIN);
 renderTemplate(menu,createSiteMenuTemplate(),RenderPosition.BEFOREEND);
 renderTemplate(filter,createSiteFilterTemplate(),RenderPosition.BEFOREEND);
 renderTemplate(sorting,createSiteSortingTemplate(),RenderPosition.BEFOREEND);
@@ -18,6 +23,7 @@ renderTemplate(sorting,createPintsContainer(),RenderPosition.BEFOREEND);
 
 const contentList = document.querySelector('.trip-events__list');
 renderTemplate(contentList,editFormTemplate(),RenderPosition.BEFOREEND);
-renderTemplate(contentList,createRoutePoint(),RenderPosition.BEFOREEND);
 
-renderTemplate(contentList,addFormTemplate(),RenderPosition.BEFOREEND);
+for (let i = 0; i < POINT_COUNT; i++ ) {
+  renderTemplate(contentList,createRoutePoint(),RenderPosition.BEFOREEND);
+}
